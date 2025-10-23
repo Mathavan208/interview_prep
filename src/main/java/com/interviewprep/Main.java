0d85dd5da1ee9e4c288598ecb777df9b96224ca7
Mathavan208 <210701154@rajalakshmi.edu.in>
2025-10-23 20:01:37 +0530

    first commit

--- src/main/java/com/interviewprep/Main.java
+++ src/main/java/com/interviewprep/Main.java
@@ -10,17 +10,14 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Get port from environment variable or use default 8080
         String port = System.getenv().getOrDefault("PORT", "8080");
         
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(Integer.parseInt(port));
         
-        // Set base directory
         String webappDirLocation = "src/main/webapp/";
         StandardContext ctx = (StandardContext) tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
         
-        // Declare compilation classpath
         File additionWebInfClasses = new File("target/classes");
         WebResourceRoot resources = new StandardRoot(ctx);
         resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
